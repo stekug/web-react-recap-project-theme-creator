2949d71e79612130257628422665495835f6a3a1
Stephan Kugler (WSL) <stephankugler.stuff@gmail.com>
2024-07-02 12:40:55 +0200

    add ColorForm component with colorAdd function

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,22 @@ import { initialColors } from './lib/colors';
 import Color from './Components/Color/Color';
 import ColorForm from './Components/ColorForm/ColorForm';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+export default function App() {
+  const [colorArray, setColorArray] = useState(initialColors);
+
+  const addColor = (newColor) => {
+    setColorArray([{ id: crypto.randomUUID(), ...newColor }, ...colorArray]);
+  };
+
   return (
     <>
       <h1>Theme Creator</h1>
-      <ColorForm />
-      {initialColors.map((color) => {
+      <ColorForm colorSubmit={addColor} />
+      {colorArray.map((color) => {
         return <Color key={color.id} color={color} />;
       })}
     </>
   );
 }
-
-export default App;
--- src/Components/ColorForm/ColorForm.jsx
+++ src/Components/ColorForm/ColorForm.jsx
@@ -1,14 +1,38 @@
+import { useState } from 'react';
 import './ColorForm.css';
 
-export default function ColorForm() {
+export default function ColorForm({ colorSubmit }) {
+  const [hexColor, setHexColor] = useState('#ff004c');
+  const [hexContrast, setHexContrast] = useState('#FFFFFF');
+
+  const updateHexColor = (event) => {
+    setHexColor(event.target.value);
+  };
+
+  const updateHexContrast = (event) => {
+    setHexContrast(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newColor = { role: event.target.role.value, hex: hexColor, contrastText: hexContrast };
+    colorSubmit(newColor);
+  };
+
   return (
-    <form className="color-form">
+    <form className="color-form" onSubmit={handleSubmit}>
+      {/* Role */}
       <label htmlFor="role">Role</label>
       <input type="text" name="role" id="role" />
-      <label htmlFor="role">Hex</label>
-      <input type="text" name="hex" id="hex" />
-      <label htmlFor="role">Contrast Text</label>
-      <input type="text" name="contrast" id="contrast" />
+      {/* Hex */}
+      <label htmlFor="hex">Hex</label>
+      <input type="text" name="hex" id="hex" value={hexColor} onChange={updateHexColor} />
+      <input type="color" name="hexColor" id="hexColor" value={hexColor} onChange={updateHexColor} />
+      {/* Contrast */}
+      <label htmlFor="contrastText">Contrast Text</label>
+      <input type="text" name="contrastText" id="contrastText" value={hexContrast} onChange={updateHexContrast} />
+      <input type="color" name="hexContrast" id="hexContrast" value={hexContrast} onChange={updateHexContrast} />
+      {/* Button */}
       <button className="color-form__button">ADD COLOR</button>
     </form>
   );
